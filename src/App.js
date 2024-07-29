90078ba24800375ba7cfeeef91d2f2f199149500
gtLee <lgt1104@gmail.com>
2024-07-29 17:24:03 +1200

    register part

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function test() {
-  let testvar = 1;
-  return console.log(testvar);
-}
+import User_Register from "./component/User_Register";
+import Main from "./component/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        test();
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<User_Register />}></Route>
+          <Route path="/Main" element={<Main />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
