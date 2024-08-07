c883439b0773ccfae2e3f98d9281cc13f8eab502
gtLee <lgt1104@gmail.com>
2024-08-07 16:30:01 +1200

    update CSS

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import '../Css/Header.css';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -11,19 +12,23 @@ export default function Header() {
     navigate("/");
   };
   return (
-    <div className="header">
-      <h1>
-        <Link to="/RecipeList">Recipe Web Page</Link>
-      </h1>
-      <div className="showid">{<p>Welcome {userinfo.userId}!!</p>}</div>
-      <div className="menu">
-        <Link to="/RecipeAdd" className="link">
-          Add Recipe
-        </Link>
-        <button onClick={logout} className="link" style={{ color: "black" }}>
-          Logout
-        </button>
+    <section id="hd_content">
+      <div className="header">
+          <h1>
+            <Link to="/RecipeList" style={{fontSize:"45px"}}>Recipe Web</Link>
+          </h1>
+          
+          <div className="menu">
+            <Link to="/RecipeAdd">
+              <button style={{ color: "black" }}>
+                Add Recipe
+              </button>
+            </Link>
+            <button onClick={logout} style={{ color: "black" }}>
+              Logout
+            </button>
+          </div>
       </div>
-    </div>
+    </section>
   );
 }
